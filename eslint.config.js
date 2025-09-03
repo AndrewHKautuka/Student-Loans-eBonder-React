93db389d32ab374bf931abe984dbfd3e013c019f
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-09-03 15:47:13 +0200

    Add import formatting to prettier
    - Add the @trivago/prettier-plugin-sort-imports package
    - Integrate the plugin into prettier and config it in the config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 import js from "@eslint/js"
-import globals from "globals"
-import tseslint from "typescript-eslint"
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
 import json from "@eslint/json"
 import markdown from "@eslint/markdown"
-import { defineConfig } from "eslint/config"
-import reactHooks from "eslint-plugin-react-hooks"
 import prettier from "eslint-plugin-prettier"
+import reactHooks from "eslint-plugin-react-hooks"
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
   {
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 import babel from "vite-plugin-babel"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 const ReactCompilerConfig = {
   /* ... */
