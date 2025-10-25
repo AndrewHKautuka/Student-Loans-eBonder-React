88456c53b1c628fbca75426e33eeb1ce3f4c8cb3
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-10-25 14:27:27 +0200

    Refactor LandingPage component to utilize new section components
    - Removed theme toggle functionality and replaced it with structured sections for improved layout and user experience
    - Integrated HeroSection, SystemOverviewSection, UserBenefitsSection, CoreFeaturesSection, GettingStartedSection, and CallToActionSection to enhance content organization and engagement

--- app/modules/public/pages/landing-page.tsx
+++ app/modules/public/pages/landing-page.tsx
@@ -1,25 +1,25 @@
-import { Label } from "@/components/ui/label"
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-import { useThemeStore } from "@/hooks/use-theme-store"
+import { Separator } from "@/components/ui/separator"
+import CallToActionSection from "@/modules/public/components/call-to-action-section"
+import CoreFeaturesSection from "@/modules/public/components/core-features-section"
+import GettingStartedSection from "@/modules/public/components/getting-started-section"
+import HeroSection from "@/modules/public/components/hero-section"
+import SystemOverviewSection from "@/modules/public/components/system-overview-section"
+import UserBenefitsSection from "@/modules/public/components/user-benefits-section"
 
 export default function LandingPage() {
-  const { theme, setTheme } = useThemeStore()
-
   return (
-    <div>
-      <Label htmlFor="theme-toggle" className="px-1 pb-2">
-        Toggle Theme:
-      </Label>
-      <ToggleGroup
-        id="theme-toggle"
-        type="single"
-        value={theme}
-        onValueChange={setTheme}
-      >
-        <ToggleGroupItem value="light">Light</ToggleGroupItem>
-        <ToggleGroupItem value="dark">Dark</ToggleGroupItem>
-        <ToggleGroupItem value="system">System</ToggleGroupItem>
-      </ToggleGroup>
+    <div className="flex flex-col gap-6 py-6 md:gap-12">
+      <HeroSection />
+      <Separator className="mx-[10%] max-w-[80%]" />
+      <SystemOverviewSection />
+      <Separator className="mx-[10%] max-w-[80%]" />
+      <UserBenefitsSection />
+      <Separator className="mx-[10%] max-w-[80%]" />
+      <CoreFeaturesSection />
+      <Separator className="mx-[10%] max-w-[80%]" />
+      <GettingStartedSection />
+      <Separator className="mx-[10%] max-w-[80%]" />
+      <CallToActionSection />
     </div>
   )
 }
